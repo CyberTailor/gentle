1d4552e4c698dd99ec4538b8a2cdc2c3ea6ed263
Anna “CyberTailor” <cyber@sysrq.in>
2023-06-07 20:14:55 +0500

    tests/generators/shards: new test

--- gentle/__main__.py
+++ gentle/__main__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: WTFPL
-# SPDX-FileCopyrightText: 2022 Anna <cyber@sysrq.in>
+# SPDX-FileCopyrightText: 2022-2023 Anna <cyber@sysrq.in>
 # No warranty
 
 import argparse
---
+++ tests/generators/shards/test_generator.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: WTFPL
+# SPDX-FileCopyrightText: 2023 Anna <cyber@sysrq.in>
+# No warranty
+
+from pathlib import Path
+
+from gentle.generators.shards import ShardsGenerator
+from gentle.metadata import MetadataXML
+
+
+def test_pkg_none(mxml: MetadataXML):
+    gen = ShardsGenerator(Path(__file__).parent / "pkg_none")
+    assert not gen.active
+
+
+def test_pkg_empty(mxml: MetadataXML):
+    gen = ShardsGenerator(Path(__file__).parent / "pkg_empty")
+    assert gen.active
+
+    mxml_old = mxml
+    gen.update_metadata_xml(mxml)
+    assert mxml_old == mxml
